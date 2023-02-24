203756a47111bb35f5ec610b0a3f61c0cda22cbf
parasshah143 <parasshah25@gmail.com>
2023-02-24 09:13:12 +0530

    test from pycharm

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from demo1_basics import demo1_login
 from demo1_basics import demo2_register
 from task1 import git_hub_task
@@ -8,7 +7,4 @@ if __name__ == '__main__':
     # demo1_login.runner()
     # demo2_register.runner()
     # git_hub_task.runner()
-    sales_force.runner()
-
-
-
+    sales_force.runner()  # start
